4dc4860ca4e98f3ff07c214e47adffb3455a029c
rongo <rongo123321@gmail.com>
2026-04-24 19:04:36 +0300

    init core ui

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.android.library) apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "AntiPhishing"
 include(":app")
- 
+include(":core:ui")
